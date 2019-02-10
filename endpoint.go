661ed62ae5d86249f151ae0daf6a3a4758e5335a
Sebastian Borza <sebito91@gmail.com>
2019-02-10 01:28:03 -0600

    updating deps to proper logrus handler

--- endpoint.go
+++ endpoint.go
@@ -1,94 +1,94 @@
 package deepstreamio
 
 import (
-    "github.com/gorilla/websocket"
-    "github.com/Sirupsen/logrus"
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
 )
 
 type endpoint struct {
-    url                 string
-    connection          *connection
+	url        string
+	connection *connection
 
-    isWebsocketClosed   chan bool
-    websocketConn       *websocket.Conn
+	isWebsocketClosed chan bool
+	websocketConn     *websocket.Conn
 }
 
 func newEndpoint(url string, connection *connection) *endpoint {
-    return &endpoint{url: url, connection: connection, isWebsocketClosed: make(chan bool, 1)}
+	return &endpoint{url: url, connection: connection, isWebsocketClosed: make(chan bool, 1)}
 }
 
 func (e *endpoint) send(msg string) {
-    logrus.WithField("msg", msg).Debug("Sent message")
-    go func() {
-        var err = e.websocketConn.WriteMessage(websocket.TextMessage, []byte(msg))
-        if err != nil {
-            e.connection.onError(err.Error())
-        }
-    }()
+	log.WithField("msg", msg).Debug("Sent message")
+	go func() {
+		var err = e.websocketConn.WriteMessage(websocket.TextMessage, []byte(msg))
+		if err != nil {
+			e.connection.onError(err.Error())
+		}
+	}()
 }
 
 func (e *endpoint) close(forceClose bool) {
-    e.isWebsocketClosed <- true
+	e.isWebsocketClosed <- true
 
-    go func() {
-        var err error
-        if forceClose {
-            err = e.websocketConn.Close()
-        } else {
-            err = e.websocketConn.WriteMessage(
-                websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-        }
+	go func() {
+		var err error
+		if forceClose {
+			err = e.websocketConn.Close()
+		} else {
+			err = e.websocketConn.WriteMessage(
+				websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		}
 
-        if err != nil {
-            e.connection.onError(err.Error())
-        } else {
-            e.connection.onClose()
-        }
-    }()
+		if err != nil {
+			e.connection.onError(err.Error())
+		} else {
+			e.connection.onClose()
+		}
+	}()
 }
 
 func (e *endpoint) open() {
-    go func() {
-        var conn, _, err = websocket.DefaultDialer.Dial(e.url, nil)
+	go func() {
+		var conn, _, err = websocket.DefaultDialer.Dial(e.url, nil)
 
-        if err != nil {
-            e.connection.onError(err.Error())
-        } else {
-            logrus.Debug("Opened endpoint")
+		if err != nil {
+			e.connection.onError(err.Error())
+		} else {
+			log.Debug("Opened endpoint")
 
-            conn.SetCloseHandler(e.websocketCloseHandler)
-            e.websocketConn = conn
+			conn.SetCloseHandler(e.websocketCloseHandler)
+			e.websocketConn = conn
 
-            e.connection.onOpen()
-            go e.readMessagesInLoop()
-        }
-    }()
+			e.connection.onOpen()
+			go e.readMessagesInLoop()
+		}
+	}()
 }
 
 func (e *endpoint) websocketCloseHandler(code int, text string) error {
-    e.isWebsocketClosed <- true
-    defer e.connection.onClose()
+	e.isWebsocketClosed <- true
+	defer e.connection.onClose()
 
-    return nil
+	return nil
 }
 
 func (e *endpoint) readMessagesInLoop() {
-    for {
-        select {
-        case <- e.isWebsocketClosed:
-            return
+	for {
+		select {
+		case <-e.isWebsocketClosed:
+			return
 
-        default:
-            var _, bytes, err = e.websocketConn.ReadMessage()
-            if err != nil {
-                e.connection.onError(err.Error())
-                return
-            }
+		default:
+			var _, bytes, err = e.websocketConn.ReadMessage()
+			if err != nil {
+				e.connection.onError(err.Error())
+				return
+			}
 
-            if rawMsg := string(bytes); len(rawMsg) > 0 {
-                logrus.WithField("msg", rawMsg).Debug("Read message")
-                e.connection.onMessage(rawMsg)
-            }            
-        }
-    }
+			if rawMsg := string(bytes); len(rawMsg) > 0 {
+				log.WithField("msg", rawMsg).Debug("Read message")
+				e.connection.onMessage(rawMsg)
+			}
+		}
+	}
 }
--- message_builder.go
+++ message_builder.go
@@ -1,46 +1,47 @@
 package deepstreamio
 
 import (
-    "strings"
-    "strconv"
-    "encoding/json"
-    "github.com/Sirupsen/logrus"
+	"encoding/json"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func getMsg(topic Topic, action Action, data []string) string {
-    c := []string{string(topic), string(action)}
-    for _, d := range data {
-        c = append(c, d)
-    }
+	c := []string{string(topic), string(action)}
+	for _, d := range data {
+		c = append(c, d)
+	}
 
-    return strings.Join(c, messageUnitSeparator) + messageRecordSeparator
+	return strings.Join(c, messageUnitSeparator) + messageRecordSeparator
 }
 
 func typed(vi interface{}) string {
-    if vi == nil {
-        return Type_Null
-    }
+	if vi == nil {
+		return Type_Null
+	}
 
-    switch v := vi.(type) {
-    case string:
-        return string(Type_String) + v
+	switch v := vi.(type) {
+	case string:
+		return string(Type_String) + v
 
-    case bool:
-        if v {
-            return string(Type_True)
-        } else {
-            return string(Type_False)
-        }
+	case bool:
+		if v {
+			return string(Type_True)
+		} else {
+			return string(Type_False)
+		}
 
-    case int:   // Need to cases for other numbers, such as uint, float etc.
-        return string(Type_Number) + strconv.Itoa(v)
+	case int: // Need to cases for other numbers, such as uint, float etc.
+		return string(Type_Number) + strconv.Itoa(v)
 
-    default:
-        b, err := json.Marshal(v)
-        if err != nil {
-            logrus.WithField("input", vi).Error("Error in marshalling object to JSON")
-            return string(Type_Undefined)
-        }
-        return string(Type_Object) + string(b)
-    }
+	default:
+		b, err := json.Marshal(v)
+		if err != nil {
+			log.WithField("input", vi).Error("Error in marshalling object to JSON")
+			return string(Type_Undefined)
+		}
+		return string(Type_Object) + string(b)
+	}
 }
